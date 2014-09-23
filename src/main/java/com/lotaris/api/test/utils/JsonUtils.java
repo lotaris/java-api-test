1d22fdd17f11de7b1818b5d33a18681ef67e166a
Laurent Prévost <laurent.prevost@lotaris.com>
2014-09-23 14:42:28 +0200

    Added utility method

--- src/main/java/com/lotaris/api/test/utils/JsonUtils.java
+++ src/main/java/com/lotaris/api/test/utils/JsonUtils.java
@@ -1,9 +1,12 @@
 package com.lotaris.api.test.utils;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 
 /**
@@ -12,7 +15,6 @@ import javax.json.JsonObjectBuilder;
  * @author Baptiste Roth <baptiste.roth@lotaris.com>
  */
 public class JsonUtils {
-
 	public static JsonObjectBuilder mapToJsonObjectBuilder(Map<String, String> map) {
 		JsonObjectBuilder jsonObject = Json.createObjectBuilder();
 		if (map == null || map.isEmpty()) {
@@ -24,5 +26,17 @@ public class JsonUtils {
 			jsonObject.add(e.getKey(), e.getValue());
 		}
 		return jsonObject;
+	}
+	
+		/**
+	 * Method used to build the JSON Array of Strings.
+	 * @return A JsonArray object
+	 */
+	private static JsonArray listToJsonArray(List<String> list) {
+		JsonArrayBuilder ab = Json.createArrayBuilder();
+			for(String element : list) {
+				ab.add(element);
+			}
+			return ab.build();
 	}
 }
